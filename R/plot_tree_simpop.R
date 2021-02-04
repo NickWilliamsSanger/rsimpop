27e7f0cc69b0d5c027d56016312d3125165a3118
Nick Williams <nw14@sanger.ac.uk>
2021-02-04 15:21:35 +0000

    Switch to additive selection model

--- R/plot_tree_simpop.R
+++ R/plot_tree_simpop.R
@@ -2,7 +2,9 @@
 #'
 #' @param tree phylo/simpop
 #' @param legpos legend position
+#' @param fmode  0=no selection coeff annotation;1=per driver selection coeff;2=composite driver selection coeff
 #' @param ... <[`dynamic-dots`][rlang::dyn-dots]> . Parameters that are passed to plot_tree
+#'
 #' @return simpop object
 #' @export
 #' @examples
@@ -10,56 +12,84 @@
 #' params=list(n_sim_days=2*365,b_stop_at_pop_size=1,b_stop_if_empty=0)
 #' growthphase=sim_pop(NULL,params=params,cfg)
 #' stree=get_subsampled_tree(growthphase,50)
-#' plot_tree_events(stree)
-plot_tree_events=function(tree,legpos="topleft",...){
-  #args=list(...)
-  events=tree$events
-  events=events[order(events$ts),]
-  if(is.null(tree$events)){
+#' plot_tree_events(stree,fmode=1)
+plot_tree_events=function (tree, legpos = "topleft", fmode=0,...)
+{
+  events = tree$events
+  events = events[order(events$ts), ]
+  if (is.null(tree$events)) {
     stop("tree has no events")
   }
-  tree=plot_tree(tree,...)
-  idx.child=match(tree$edge[,2],tree$edge[,1])
-  N=dim(tree$edge)[1]
-  TT=max(tree$timestamp)
-  duration=ifelse(is.na(idx.child),TT-tree$tBirth,tree$tBirth[idx.child]-tree$tBirth)
-  df=data.frame(uval=unique(sort(sprintf("%s:%s",events$value,events$driverid))),stringsAsFactors = FALSE)
-  cols=c(RColorBrewer::brewer.pal(9,"Set1"))#,RColorBrewer::brewer.pal(12,"Set3"))
-  df$col=c(cols,cols,cols,cols)[1:length(df$uval)]
-  df$pch=rep(c(19,17,15,25),each=length(cols))[1:length(df$uval)]
+  tree = plot_tree(tree, ...)
+  idx.child = match(tree$edge[, 2], tree$edge[, 1])
+  N = dim(tree$edge)[1]
+  TT = max(tree$timestamp)
+  duration = ifelse(is.na(idx.child), TT - tree$tBirth, tree$tBirth[idx.child] -
+                      tree$tBirth)
+  events$fitness=tree$cfg$drivers$fitness[match(events$driver,tree$cfg$drivers$driver)]
+  events$fitness[is.na(events$fitness)]=0
+  if(fmode==2){
+    warn("WARNING: fmode=2 under development gives wrong results for multiple drivers on single branch")
+    s=sapply(1:length(events$driverid),function(i){
+      if(events$driverid[i]>0){
+
+        parents=intersect(rsimpop:::get_parents(events$node[i],tree$edge),events$node)
+        idx=which(events$node %in% parents)
+        #sum(events$fitness[idx])
+        sum(events$fitness[idx])
+      }else{
+        0
+      }
+    })
+    events$fitness=s
+  }
+  if(fmode>0){
+    events$key=sprintf("%s:%s:%3.2f", events$value,
+                     events$driverid,events$fitness)
+  }else{
+    events$key=sprintf("%s:%s", events$value,
+                       events$driverid)
+  }
+  df = data.frame(uval = unique(sort(events$key)), stringsAsFactors = FALSE)
+  cols = c(RColorBrewer::brewer.pal(9, "Set1"))
+  df$col = c(cols, cols, cols, cols)[1:length(df$uval)]
+  df$pch = rep(c(19, 17, 15, 25), each = length(cols))[1:length(df$uval)]
   print(df)
-  events=events %>% mutate(key=sprintf("%s:%s",value,driverid)) %>% left_join(df,by=c("key"="uval"))
-  events$idx=match(events$node,tree$edge[,2])
-  fracs=lapply(1:N,function(x) c())
-  cols =lapply(1:N,function(x) c())
-  for(i in 1:length(events$node)){
-    thisnode=events$node[i]
-    idx=events$idx[i]
-    info=get_edge_info(NULL,tree,thisnode)
-    frac=(events$ts[i]-tree$tBirth[events$idx[i]])/duration[events$idx[i]]
-    points(x=info$x,y=info$yt-frac*(info$yt-info$yb),pch=events$pch[i],col=events$col[i],cex=2)
-    fracs[[idx]]=c(fracs[[idx]],frac)
-    cols[[idx]]=c(cols[[idx]],events$col[i])
-    kids=get_all_node_children(thisnode,tree)
-    for(k in match(kids,tree$edge[,2])){
-        fracs[[k]]=0
-        cols[[k]]=events$col[i]
+  events = events %>% left_join(df, by = c(key = "uval"))
+
+  events$idx = match(events$node, tree$edge[, 2])
+  print(events)
+  fracs = lapply(1:N, function(x) c())
+  cols = lapply(1:N, function(x) c())
+
+  for (i in 1:length(events$node)) {
+    thisnode = events$node[i]
+    idx = events$idx[i]
+    info = rsimpop:::get_edge_info(NULL, tree, thisnode)
+    frac = (events$ts[i] - tree$tBirth[events$idx[i]])/(duration[events$idx[i]]+1e-6)##Fix zero duration
+    points(x = info$x, y = info$yt - frac * (info$yt - info$yb),
+           pch = events$pch[i], col = events$col[i], cex = 2)
+    fracs[[idx]] = c(fracs[[idx]], frac)
+    cols[[idx]] = c(cols[[idx]], events$col[i])
+    kids = get_all_node_children(thisnode, tree)
+    for (k in match(kids, tree$edge[, 2])) {
+      fracs[[k]] = 0
+      cols[[k]] = events$col[i]
     }
-
   }
-  for(j in 1:N){
-    info=get_edge_info(NULL,tree,tree$edge[j,2])
-    ff=c(fracs[[j]][-1],1)
-    segments(x0=info$x,y1=info$yt-fracs[[j]]*(info$yt-info$yb),
-             y0=info$yt-ff*(info$yt-info$yb),col=cols[[j]],lwd=1)
-
+  for (j in 1:N) {
+    info = rsimpop:::get_edge_info(NULL, tree, tree$edge[j, 2])
+    ff = c(fracs[[j]][-1], 1)
+    segments(x0 = info$x, y1 = info$yt - fracs[[j]] * (info$yt -
+                                                         info$yb), y0 = info$yt - ff * (info$yt - info$yb),
+             col = cols[[j]], lwd = 1)
   }
-  if(!is.null(legpos)){
-  legend(legpos,legend=df$uval,col=df$col,pch=df$pch,cex=1)
+  if (!is.null(legpos)) {
+    legend(legpos, legend = df$uval, col = df$col, pch = df$pch,
+           cex = 1)
   }
   tree
 }
-
 
 get_tree_overlap=function(tree,selsim){
   st=tree
@@ -191,6 +221,12 @@ plot_tree_overlaps=function(tree,legpos="topleft",scale=1,b.add.count=TRUE,...){
   tree
 }
 
+#' Adds a vector "dcount" to a phylo object giving the number of descendent tips for each child node
+#'
+#' @param tree id of specified node
+#' @param tree phylo
+#' @return phylo with dcount vector in parallel to edge.length
+#' @export
 add_descendent_counts_to_tree=function(tree){
   N=length(tree$tip.label)
   idx=which(tree$edge[,2]<=N)
